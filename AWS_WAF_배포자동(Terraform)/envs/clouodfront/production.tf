478a44b5243afd306a79cc8d1c28bad651a359e8
jaehwanjoa <90813478+jaehwanjoa@users.noreply.github.com>
2026-01-21 13:21:20 +0900

    Add files via upload

---
+++ AWS_WAF_배포자동(Terraform)/envs/clouodfront/production.tf
@@ -0,0 +1,11 @@
+module "cloudfront_waf" {
+  source = "../../modules/waf"
+
+  providers = {
+    aws = aws.global
+  }
+
+  waf_name   = var.waf_name
+  waf_scope  = var.waf_scope
+  waf_s3_arn = var.waf_s3_arn
+}
